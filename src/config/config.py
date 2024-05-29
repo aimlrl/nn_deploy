e8c2ede4fd89eae990964ec82c23a21c303fedb4
aimlrl <mlresearch.axis@gmail.com>
2024-05-29 12:19:47 +0530

    Added config package and it's module

---
+++ src/config/config.py
@@ -0,0 +1,24 @@
+NUM_INPUTS = 2
+NUM_LAYERS = 3
+P = [NUM_INPUTS,2,1]
+
+f = [None,"Linear","Sigmoid"]
+
+LOSS_FUNCTION = "Mean Squared Error"
+MINI_BATCH_SIZE = 1
+
+
+#theta0 = [None]
+#theta = [None]
+
+#z = [None]*NUM_LAYERS
+#h = [None]*NUM_LAYERS
+
+"""
+del_fl_by_del_z = [None]*NUM_LAYERS
+del_hl_by_del_theta0 = [None]*NUM_LAYERS
+del_hl_by_del_theta = [None]*NUM_LAYERS
+del_L_by_del_h = [None]*NUM_LAYERS
+del_L_by_del_theta0 = [None]*NUM_LAYERS
+del_L_by_del_theta = [None]*NUM_LAYERS
+"""

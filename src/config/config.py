101267f1bab775ef13ecfdb7541854afabad8011
aimlrl <mlresearch.axis@gmail.com>
2024-05-30 16:06:58 +0530

    Modified config.py and added data_management.py

--- src/config/config.py
+++ src/config/config.py
@@ -1,3 +1,7 @@
+import os
+import pathlib
+import src
+
 NUM_INPUTS = 2
 NUM_LAYERS = 3
 P = [NUM_INPUTS,2,1]
@@ -7,6 +11,11 @@ f = [None,"Linear","Sigmoid"]
 LOSS_FUNCTION = "Mean Squared Error"
 MINI_BATCH_SIZE = 1
 
+PACKAGE_ROOT = pathlib.Path(src.__file__).resolve().parent
+DATAPATH = os.path.join(PACKAGE_ROOT,"datasets") 
+#"/src/datasets"
+SAVED_MODEL_PATH = os.path.join(PACKAGE_ROOT,"trained_models")
+#"/src/trained_models"
 
 #theta0 = [None]
 #theta = [None]
---
+++ src/preprocessing/data_management.py
@@ -0,0 +1,36 @@
+import os
+import pandas as pd
+import pickle
+
+from src.config import config
+
+
+def load_dataset(file_name):
+
+    file_path = os.path.join(config.DATAPATH,file_name)
+    #"/src/datasets/file_name"
+
+    data = pd.read_csv(file_path)
+
+    return data
+
+
+def save_model(theta0,theta):
+
+    pkl_file_path = os.path.join(config.SAVED_MODEL_PATH,"two_input_xor_nn.pkl")
+
+    with open(pkl_file_path,"wb") as file_handle:
+        file_handle.dump({"params":{"biases":theta0,"weights":theta},"activations":config.f})
+
+
+def load_model(file_name):
+
+    pkl_file_path = os.path.join(config.SAVED_MODEL_PATH,file_name)
+
+    with open(pkl_file_path,"rb") as file_handle:
+        loaded_model = file_handle.load()
+
+    return loaded_model
+
+
+    
